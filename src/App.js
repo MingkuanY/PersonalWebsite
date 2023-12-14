b7e39195e372cad7b3c9fdadcfbd90640eca652a
Mingkuan Yan <mingkuany@gmail.com>
2023-12-14 00:12:51 -0800

    Starting on an empty slate with components outlined.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import Professional from "./components/Professional/Professional";
+import Travel from "./components/Travel/Travel";
+import Music from "./components/Music/Music";
+import Photography from "./components/Photography/Photography";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Professional />
+      <Travel />
+      <Music />
+      <Photography />
     </div>
   );
 }
