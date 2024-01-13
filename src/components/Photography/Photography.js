2677da92c2d3b98e174f54ff81375da3c32e3ef2
Mingkuan Yan <mingkuany@gmail.com>
2024-01-13 17:08:36 -0500

    Start with a clean slate

--- src/components/Photography/Photography.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Photography() {
-  return (
-    <div>Photography</div>
-  )
-}
-
-export default Photography
--- src/components/Professional/Professional.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Professional() {
-  return (
-    <div>Professional</div>
-  )
-}
-
-export default Professional
